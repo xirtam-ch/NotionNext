b9c987a722d2f21e811100f4bff32f59d39afde6
xirtam <751348449@qq.com>
2025-12-07 09:01:23 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '人生无意义，所以在可以被赋予各种意义', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://wr.xirtam.bar', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://wr.xirtam.click', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
